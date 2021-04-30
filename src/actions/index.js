df79b270db923dd0c5ad6af6fe4d3bc6f366e891
Karina Sanchez <76711593+ksanchez1234@users.noreply.github.com>
2021-04-30 11:40:46 -0500

    Add files via upload

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+// Setting Profile Pic Actions
+export const setPicture = () => {
+    return{
+        type: 'SETPICTURE'
+    };
+};
+export const noPicture = () => {
+    return{
+        type: 'NOPICTURE'
+    };
+};
+//Setting User Name Actions
+export const setUserName = () => {
+    return{
+        type: 'SETUSERNAME'
+    };
+};
+export const noUserName = () => {
+    return{
+        type: 'NOUSERNAME'
+    };
+};
---
+++ src/pages/Stuff.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Stuff() {
+    return (
+        <div className='stuff'>
+            <h1>Check out some stuff!</h1>
+        </div>
+    )
+}
+
+export default Stuff
